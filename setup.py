c9a299e001e83730efbe5906ce24884bedd8f31e
fbartolic <fb90@st-andrews.ac.uk>
2022-01-21 10:10:48 +0000

    minor

--- setup.py
+++ setup.py
@@ -55,10 +55,12 @@ class CMakeBuildExt(build_ext):
             "-DPython_INCLUDE_DIRS={}".format(cmake_python_include_dir),
             "-DCMAKE_BUILD_TYPE={}".format("Debug" if self.debug else "Release"),
             "-DCMAKE_PREFIX_PATH={}".format(pybind11.get_cmake_dir()),
-            "-DCMAKE_CXX_COMPILER=g++",
+#            "-DCMAKE_CXX_COMPILER=g++",
         ]
         if os.environ.get("EHRLICH_ABERTH_JAX_CUDA", "no").lower() == "yes":
             cmake_args.append("-DEHRLICH_ABERTH_JAX_CUDA=yes")
+#            cmake_args.append("-DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.2/bin/nvcc")
+
 
         os.makedirs(self.build_temp, exist_ok=True)
         subprocess.check_call(["cmake", HERE] + cmake_args, cwd=self.build_temp)

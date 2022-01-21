d7a0c717f04dba36efc873ae07ff7874f1dcb725
fbartolic <fb90@st-andrews.ac.uk>
2022-01-21 10:22:15 +0000

    hardcode cuda req.

--- setup.py
+++ setup.py
@@ -55,10 +55,11 @@ class CMakeBuildExt(build_ext):
             "-DPython_INCLUDE_DIRS={}".format(cmake_python_include_dir),
             "-DCMAKE_BUILD_TYPE={}".format("Debug" if self.debug else "Release"),
             "-DCMAKE_PREFIX_PATH={}".format(pybind11.get_cmake_dir()),
-#            "-DCMAKE_CXX_COMPILER=g++",
+            "-DCMAKE_CXX_COMPILER=g++",
         ]
-        if os.environ.get("EHRLICH_ABERTH_JAX_CUDA", "no").lower() == "yes":
-            cmake_args.append("-DEHRLICH_ABERTH_JAX_CUDA=yes")
+        cmake_args.append("-DEHRLICH_ABERTH_JAX_CUDA=yes")
+#        if os.environ.get("EHRLICH_ABERTH_JAX_CUDA", "no").lower() == "yes":
+#            cmake_args.append("-DEHRLICH_ABERTH_JAX_CUDA=yes")
 #            cmake_args.append("-DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.2/bin/nvcc")
 
 
@@ -84,16 +85,16 @@ extensions = [
     Extension("ehrlich_aberth_jax.cpu_ops", ["src/ehrlich_aberth_jax/src/cpu_ops.cc"],),
 ]
 
-if os.environ.get("EHRLICH_ABERTH_JAX_CUDA", "no").lower() == "yes":
-   extensions.append(
-       Extension(
-           "ehrlich_aberth_jax.gpu_ops",
-           [
-               "src/ehrlich_aberth_jax/src/gpu_ops.cc",
-               "src/ehrlich_aberth_jax/src/cuda_kernels.cc.cu",
-           ],
-       )
-   )
+#if os.environ.get("EHRLICH_ABERTH_JAX_CUDA", "no").lower() == "yes":
+extensions.append(
+    Extension(
+        "ehrlich_aberth_jax.gpu_ops",
+        [
+            "src/ehrlich_aberth_jax/src/gpu_ops.cc",
+            "src/ehrlich_aberth_jax/src/cuda_kernels.cc.cu",
+        ],
+    )
+)
 
 setup(
     name="ehrlich_aberth_jax",
